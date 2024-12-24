3527a30194c98027c1a23bef024ff0bbd4dc1b32
Zhenbin Lin <lin-zhenbin@qq.com>
2024-12-24 16:53:35 +0800

    migrate test transformers privateuse1 functions

--- test/test_transformers_privateuse1.py
+++ test/test_transformers_privateuse1.py
@@ -22,6 +22,86 @@ from torch.testing._internal.common_utils import (
 SdpaShape = namedtuple("Sdpa_Shape", ["batch", "num_heads", "seq_len", "head_dim"])
 
 
+def _scaled_dot_product_fused_attention_overrideable(
+    query: torch.Tensor,
+    key,
+    value,
+    attn_bias=None,
+    dropout_p=None,
+    is_causal=None,
+    return_debug_mask=None,
+    scale=None,
+):
+    batch_size = query.size(0)
+    num_heads = query.size(1)
+    head_dim_v = value.size(3)
+    max_seqlen_q = query.size(2)
+    max_seqlen_kv = key.size(2)
+
+    dtype = query.dtype
+    device = query.device
+    layout = query.layout
+    output = torch.empty(
+        (batch_size, num_heads, max_seqlen_q, head_dim_v),
+        dtype=dtype,
+        layout=layout,
+        device=device,
+    )
+    logsumexp = torch.empty(
+        (batch_size, num_heads, max_seqlen_q),
+        dtype=torch.float,
+        layout=layout,
+        device=device,
+    )
+    debug_attn_mask = torch.empty(
+        (batch_size, num_heads, max_seqlen_q, max_seqlen_kv),
+        dtype=torch.float,
+        layout=layout,
+        device=device,
+    )
+
+    philox_seed = torch.empty((), dtype=torch.long)
+    philox_offset = torch.empty((), dtype=torch.long)
+    return (
+        output,
+        logsumexp,
+        torch.tensor(()),
+        torch.tensor(()),
+        max_seqlen_q,
+        max_seqlen_kv,
+        philox_seed,
+        philox_offset,
+        debug_attn_mask,
+    )
+
+
+def _scaled_dot_product_fused_attention_overrideable_backward(
+    grad_out,
+    query,
+    key,
+    value,
+    attn_bias,
+    grad_input_mask,
+    out,
+    logsumexp,
+    cum_seq_q,
+    cum_seq_k,
+    imax_q,
+    imax_k,
+    dropout_p,
+    is_causal,
+    philox_seed,
+    philox_offset,
+    scale=None,
+):
+    return (
+        torch.empty_like(query),
+        torch.empty_like(key),
+        torch.empty_like(value),
+        torch.empty_like(attn_bias),
+    )
+
+
 @unittest.skipIf(TEST_XPU, "XPU does not support cppextension currently")
 @unittest.skipIf(
     IS_FBCODE,
@@ -40,6 +120,22 @@ class TestSDPAPrivateUse1Only(NNTestCase):
             extra_cflags=["-g"],
             verbose=True,
         )
+
+        cls.lib = torch.library.Library("aten", "IMPL")  # noqa: TOR901
+        cls.lib.impl(
+            "_scaled_dot_product_fused_attention_overrideable",
+            _scaled_dot_product_fused_attention_overrideable,
+            dispatch_key="PrivateUse1",
+        )
+        cls.lib.impl(
+            "_scaled_dot_product_fused_attention_overrideable_backward",
+            _scaled_dot_product_fused_attention_overrideable_backward,
+            dispatch_key="PrivateUse1",
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.lib._destroy()
 
     @skipIfTorchDynamo()
     def test_fused_sdp_choice_privateuseone(self):
